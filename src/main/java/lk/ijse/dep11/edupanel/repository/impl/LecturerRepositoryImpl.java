652f139dc5d68015e9580a2df43abcc3bde78173
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-04-16 15:31:09 +0530

    Finalize the CrudRepositoryImpl

--- src/main/java/lk/ijse/dep11/edupanel/repository/impl/LecturerRepositoryImpl.java
+++ src/main/java/lk/ijse/dep11/edupanel/repository/impl/LecturerRepositoryImpl.java
@@ -1,54 +1,10 @@
 package lk.ijse.dep11.edupanel.repository.impl;
 
 import lk.ijse.dep11.edupanel.entity.Lecturer;
+import lk.ijse.dep11.edupanel.repository.CrudRepositoryImpl;
 import lk.ijse.dep11.edupanel.repository.custom.LecturerRepository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-import java.util.Optional;
 
-public class LecturerRepositoryImpl implements LecturerRepository {
-    private EntityManager em;
-    @Override
-    public Lecturer save(Lecturer entity) {
-        em.persist(entity);
-        return entity;
-    }
-
-    @Override
-    public void update(Lecturer entity) {
-        em.merge(entity);
-    }
-
-    @Override
-    public void deleteById(Integer pk) {
-        em.remove(em.find(Lecturer.class, pk));
-    }
-
-    @Override
-    public boolean existsById(Integer pk) {
-        return findById(pk).isPresent();
-    }
-
-
-    @Override
-    public Optional<Lecturer> findById(Integer pk) {
-        return Optional.ofNullable(em.find(Lecturer.class, pk));
-    }
-
-    @Override
-    public List<Lecturer> findAll() {
-        return em.createQuery("SELECT l From Lecturer l",Lecturer.class).getResultList();
-    }
-
-    @Override
-    public long count() {
-        return em.createQuery("SELECT count(l) FROM Lecturer l", Long.class).getSingleResult();
-    }
-
-    @Override
-    public void setEntityManager(EntityManager em) {
-        this.em = em;
-    }
+public class LecturerRepositoryImpl extends CrudRepositoryImpl<Lecturer,Integer> implements LecturerRepository {
 
 }
--- src/main/java/lk/ijse/dep11/edupanel/repository/impl/LinkedInRepositoryImpl.java
+++ src/main/java/lk/ijse/dep11/edupanel/repository/impl/LinkedInRepositoryImpl.java
@@ -12,47 +12,5 @@ import java.util.Optional;
 
 public class LinkedInRepositoryImpl implements LinkedInRepository {
 
-    private EntityManager em;
-
-    @Override
-    public void setEntityManager(EntityManager em) {
-        this.em = em;
-    }
-
-    @Override
-    public LinkedIn save(LinkedIn entity) {
-        em.persist(entity);
-        return entity;
-    }
-
-    @Override
-    public void update(LinkedIn entity) {
-        em.merge(entity);
-    }
-
-    @Override
-    public void deleteById(Lecturer pk) {
-        em.remove(em.find(LinkedIn.class, pk));
-    }
-
-    @Override
-    public boolean existsById(Lecturer pk) {
-        return findById(pk).isPresent();
-    }
-
-    @Override
-    public Optional<LinkedIn> findById(Lecturer pk) {
-        return Optional.ofNullable(em.find(LinkedIn.class, pk));
-    }
-
-    @Override
-    public List<LinkedIn> findAll() {
-        return em.createQuery("SELECT li FROM LinkedIn li", LinkedIn.class).getResultList();
-    }
-
-    @Override
-    public long count() {
-        return em.createQuery("SELECT COUNT(li) FROM LinkedIn li", Long.class).getSingleResult();
-    }
 
 }
